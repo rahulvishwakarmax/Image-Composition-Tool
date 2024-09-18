80d69cc2debbc2abf14f2862304883fad652466e
rahulvishwakarmax <vishwakarmarahul2411@gmail.com>
2024-09-18 14:44:26 +0530

    Update script.js
    Added image file upload feature, integrated drag-and-drop functionality for canvas, improved event handling for drawing actions.

--- script.js
+++ script.js
@@ -19,40 +19,32 @@ function clearCanvases() {
     var canvas1 = document.getElementById("can1");
     var context1 = canvas1.getContext("2d");
     context1.clearRect(0, 0, canvas1.width, canvas1.height);
-
     var canvas2 = document.getElementById("can2");
     var context2 = canvas2.getContext("2d");
     context2.clearRect(0, 0, canvas2.width, canvas2.height);
 }
 
 function createComposite() {
-    if (fgimg == null || !fgimg.complete()) {
+    if (!fgimg || !fgimg.complete()) {
         alert("Upload foreground image");
-        return; // Return early if foreground image is not uploaded
+        return;
     }
-
-    if (bgimg == null || !bgimg.complete()) {
+    if (!bgimg || !bgimg.complete()) {
         alert("Upload background image");
-        return; // Return early if background image is not uploaded
+        return;
     }
-
     bgimg.setSize(fgimg.width, fgimg.height);
     var outputimg = new SimpleImage(fgimg.width, fgimg.height);
-
     for (var pixel of fgimg.values()) {
         var x = pixel.getX();
         var y = pixel.getY();
-        var opixel = outputimg.getPixel(x, y);
-
         if (pixel.getGreen() > pixel.getRed() + pixel.getBlue()) {
             outputimg.setPixel(x, y, bgimg.getPixel(x, y));
         } else {
             outputimg.setPixel(x, y, pixel);
         }
     }
-
     clearCanvases();
-
     var canvas = document.getElementById("can1");
     outputimg.drawTo(canvas);
 }
@@ -60,7 +52,6 @@ function createComposite() {
 function trySampleImages(sampleNumber) {
     var fgSampleImage = new Image();
     var bgSampleImage = new Image();
-
     if (sampleNumber === 1) {
         fgSampleImage.src = "drewRobert.png"; 
         bgSampleImage.src = "dinos.png";
@@ -71,7 +62,6 @@ function trySampleImages(sampleNumber) {
         fgSampleImage.src = "try33.jpeg"; 
         bgSampleImage.src = "try3.jpeg";
     }
-
     fgSampleImage.onload = function () {
         var canvas1 = document.getElementById("can1");
         canvas1.width = fgSampleImage.width; 
@@ -84,7 +74,7 @@ function trySampleImages(sampleNumber) {
         canvas2.height = bgSampleImage.height; 
         var ctx2 = canvas2.getContext("2d");
         ctx2.drawImage(bgSampleImage, 0, 0);
-
+        
         fgimg = new SimpleImage(fgSampleImage);
         bgimg = new SimpleImage(bgSampleImage);
     };
